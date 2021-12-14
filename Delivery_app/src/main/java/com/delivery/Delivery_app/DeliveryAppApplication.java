9a8572c5f044841c11a51838869bf3937e938044
Suraj Verma <vermasuraj0008@gmail.com>
2021-12-14 19:06:46 +0530

    adding the delivery starter file

---
+++ Delivery_app/src/main/java/com/delivery/Delivery_app/DeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.Delivery_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAppApplication.class, args);
+	}
+
+}
