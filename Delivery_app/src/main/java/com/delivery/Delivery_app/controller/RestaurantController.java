3e11e81ad36348d4f4527238520a119f14a988c1
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-12-15 16:16:22 +0530

    Restaurant Endpoints ready with Crud operation

---
+++ Delivery_app/src/main/java/com/delivery/Delivery_app/controller/RestaurantController.java
@@ -0,0 +1,46 @@
+package com.delivery.Delivery_app.controller;
+
+import com.delivery.Delivery_app.entity.Restaurant;
+import com.delivery.Delivery_app.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+//    get all restaurant endpoint
+    @GetMapping("/restaurants")
+    public List<Restaurant> getAllRestaurants(){
+        return restaurantService.getAllRestaurants();
+    }
+
+//    get restaurant endpoint
+    @GetMapping("/restaurant/{id}")
+    public Restaurant getRestaurant(@PathVariable("id") long restaurant_id){
+        return restaurantService.getRestaurant(restaurant_id);
+    }
+
+//    add restaurant endpoint
+    @PostMapping("/restaurant")
+    public Restaurant addRestaurant(@RequestBody Restaurant restaurant){
+        return restaurantService.addRestaurant(restaurant);
+    }
+//    update existing restaurant
+    @PutMapping("/restaurant")
+    public String updateRestaurant(@RequestBody Restaurant restaurant){
+        return restaurantService.updateRestaurant(restaurant);
+    }
+//  Delete restaurant from existing db
+    @DeleteMapping("/restaurant/{id}")
+    public String deleteRestaurant(@PathVariable("id") long restaurant_id ){
+        boolean flag = restaurantService.deleteRestaurant(restaurant_id);
+        if(flag) return "Delete Restaurant";
+        else return "Delete Failed";
+    }
+
+}
